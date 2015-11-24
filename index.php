b60d47707dea47a6edbfa5d3bc97997c2218d803
Achintha Gunasekara <achintha@me.com>
2015-11-25 09:10:14 +1100

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
  */
  
 require_once('config.ini.php'); //include the base configuration path
-//setup the environment
-require_once(BASE_PATH.'/code/setup.inc.php');
-
+require_once(BASE_PATH.'/code/setup.inc.php'); //setup the environment
 include "header.php";
 include "code/" . $page . ".php";
 include "footer.php";
